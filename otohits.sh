da1dcdba5c5d939b178945fad38a67a9f30ac07b
Krishna Chaitanya <capriciousduck@gmail.com>
2020-10-25 11:04:00 +0530

    Delete otohits.sh

--- otohits.sh
+++
@@ -1,8 +0,0 @@
-wget https://cdndl.otohits.net/dl/OtohitsApp_5063_linux_portable.tar.gz > /dev/null
-mkdir OtohitsApp
-cd OtohitsApp
-echo "/login:cb87be4b-9e5e-4dbb-93e8-3f475e4d9a75" > otohits.ini
-echo "/nosandbox" >> otohits.ini
-tar -xzf ../OtohitsApp_5063_linux_portable.tar.gz
-
-./otohits-app
